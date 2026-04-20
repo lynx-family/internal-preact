4dfde81653cc74da5e4ba4fde59d4f7e2ebcedd1
Yiming Li <yimingli.cs@gmail.com>
2026-04-20 17:07:34 +0800

    fix: add optional chaining for __nextSlotIndex (#8)
    (cherry picked from commit edd1f6ca7b9f75baa69e8ae375f0875b81fb0339)

--- src/diff/children.js
+++ src/diff/children.js
@@ -405,7 +405,7 @@ function insert(parentVNode, oldDom, parentDom, isMounting) {
 		}
 
 		return oldDom;
-	} else if (parentVNode._dom.__nextSlotIndex != parentVNode._dom.__slotIndex) {
+	} else if (parentVNode._dom?.__nextSlotIndex != parentVNode._dom?.__slotIndex) {
 		parentVNode._dom.__slotIndex = parentVNode._dom.__nextSlotIndex;
 		parentDom.insertBefore(
 			parentVNode._dom,
